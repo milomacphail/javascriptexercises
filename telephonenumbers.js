d2ffe8f6d4e71a5051112ae8e1cc3cfffe95b857
Milo MacPhail <milomacphail@gmail.com>
2018-10-13 16:46:41 +0000

    Telephone number

---
+++ telephonenumbers.js
@@ -0,0 +1,6 @@
+function telephoneCheck(str) {
+    var regCheck = /^(1\\s?)?(\\(\\d{3}\\)|\\d{3})[\\s\\-]?\\d{3}[\\s\\-]?\\d{4}$/;
+    return regCheck.test(str);
+    return true;}
+    
+    telephoneCheck("555-555-5555")
