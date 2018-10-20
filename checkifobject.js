d4ae5e92093715f883235af06447537f8a30c69e
Milo MacPhail <milomacphail@gmail.com>
2018-10-20 14:37:53 +0000

    Added CheckIfObject

---
+++ checkifobject.js
@@ -0,0 +1,13 @@
+let users = {
+    Alan: {age: 27, online: true}, 
+    Jeff: {age: 32, online: true}, 
+    Sarah: {age: 48, online: true}, 
+    Ryan: { age: 19, online: true}
+    
+}; 
+function isEveryoneHere(obj) {
+    if (users.hasOwnProperty('Alan','Jeff','Sarah','Ryan')) 
+    { return true; } else { return false; }
+    }; 
+
+console.log(isEveryoneHere(users));
