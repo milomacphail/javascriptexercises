1a7e26c33eb9228487150adcc120f17141a69ca8
milomacphail <milomacphail@gmail.com>
2020-04-06 18:05:15 -0400

    Classwork done - app done

---
+++ react-app-tutorial/tutorial-2/src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      pageNum: 1,
+      totalPages: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.changePage(this.state.pageNum);
+  }
+
+  changePage() {
+    const url = `https://reqres.in/api/users?page=${this.state.pageNum}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => this.setState({ users: res.data, pageNum: this.pageNum }));
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Users list:</h1>
+        </div>
+        <div className="users">
+          {users.map((user) => (
+            <div className="card">
+              <img
+                className="card-img-top"
+                src={user.avatar}
+                key={user.id}
+                alt="User email avatar"
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {user.first_name}
+                  {user.last_name}
+                </h5>
+                <p className="card-text">{user.email}</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="btn-group" aria-label="Page navigation example">
+          <button
+            disabled={this.state.pageNum === 1 ? true : false}
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => this.changePage(this.state.pageNum - 1)}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => this.changePage(this.state.pageNum + 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
